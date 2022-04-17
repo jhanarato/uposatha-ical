f0caf2e1f099271f119568c1d73ed68c04172c1b
Ajahn Jhanarato <jhanarato@gmail.com>
2022-04-17 11:58:56 +0800

    A simple example program to show the details of the upcoming uposatha at the command line.

---
+++ uposatha-cli.py
@@ -0,0 +1,37 @@
+# My minimum viable product.
+import icalendar
+from forest_sangha_moons import MahanikayaCalendar
+
+with open("mahanikaya.ical", "r") as f:
+    content = f.read()
+
+ical = icalendar.Calendar.from_ical(content)
+calendar = MahanikayaCalendar()
+calendar.import_ical(ical)
+
+if calendar.today_is_uposatha():
+    print("Today is the uposatha")
+
+print("Days until next uposatha: {}".format(
+    calendar.days_to_next_uposatha()
+))
+
+next_uposatha = calendar.next_uposatha()
+
+print("The next uposatha is a {} day {} moon on {}".format(
+    next_uposatha.uposatha_days,
+    next_uposatha.moon_name,
+    next_uposatha.date.strftime("%A, %d %b %y")
+))
+
+print("It is uposatha {} of {} of the {} season".format(
+    next_uposatha.uposatha_of_season,
+    next_uposatha.season.uposatha_count,
+    next_uposatha.season.season_name
+))
+
+special = next_uposatha.special_day
+
+if special:
+    print("It is: {}".format(special))
+
