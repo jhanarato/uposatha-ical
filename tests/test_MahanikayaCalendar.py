e82c47ca09ab5b9af6fdd75ce59953e1e44010ff
Ajahn Jhanarato <jhanarato@gmail.com>
2022-11-27 10:11:11 +0800

    Removed imports in __init__.py

--- tests/test_MahanikayaCalendar.py
+++ tests/test_MahanikayaCalendar.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
 from datetime import date
 
-from forest_sangha_moons import MahanikayaCalendar, Event, Season
-from forest_sangha_moons.MahanikayaCalendar import ExtendedSummary, SeasonMaker
+from forest_sangha_moons.MahanikayaCalendar import Event, Season, ExtendedSummary, SeasonMaker, MahanikayaCalendar
 
 
 def details_to_seasons(details):
