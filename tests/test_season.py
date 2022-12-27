54426fdd91e93ec7925e9b209b53a5b950194926
Ajahn Jhanarato <jhanarato@gmail.com>
2022-12-27 10:18:39 +0800

    test_end_date() uses first_month_of_cold_season()

--- tests/test_season.py
+++ tests/test_season.py
@@ -1,16 +1,10 @@
 from datetime import date
 
 from forest_sangha_moons.MahanikayaCalendar import Season
-from conftest import details_to_seasons
+from conftest import details_to_seasons, first_month_of_cold_season
 
 def test_end_date():
-    details = [
-        {"date": date(2010, 11, 29), "summary": "Waning Moon"},
-        {"date": date(2010, 12, 6), "summary": "New Moon - 15 day Hemanta 1/8"},
-        {"date": date(2010, 12, 14), "summary": "Waxing Moon"},
-        {"date": date(2010, 12, 21), "summary": "Full Moon - 15 day Hemanta 2/8"}
-    ]
-
+    details = first_month_of_cold_season()
     season = details_to_seasons(details)[0]
     assert season.end_date() == date(2010, 12, 21)
 
