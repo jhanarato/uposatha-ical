5d4131eea5203c60860f9bc79162782b97d532f6
Ajahn Jhanarato <jhanarato@gmail.com>
2022-12-28 17:33:19 +0800

    Made quick script to import icalendar file to be used exploring data in REPL.

---
+++ examples/quick_icalendar_import.py
@@ -0,0 +1,11 @@
+import icalendar
+from forest_sangha_moons.MahanikayaCalendar import MahanikayaCalendar
+
+def import_calendar():
+    """ For use in python console. Quickly import the icalendar file for exploration """
+    with open("mahanikaya.ical", "r") as f:
+        content = f.read()
+
+    ical = icalendar.Calendar.from_ical(content)
+    calendar = MahanikayaCalendar()
+    calendar.import_ical(ical)
