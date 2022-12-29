66e1854bdffb9b6b009c38a077d3ed851c1bf6bf
Ajahn Jhanarato <jhanarato@gmail.com>
2022-12-29 10:24:02 +0800

    Added a bit of whitespace.

--- tests/generate.py
+++ tests/generate.py
@@ -23,7 +23,9 @@ def generate_season_with_one_event(event_date):
 def generate_uposatha_dates(day_before_season):
     lengths = iter(uposatha_lengths())
     next_date = day_before_season + timedelta(days=next(lengths))
+
     yield next_date
+
     for length in lengths:
         next_date += timedelta(days=length)
         yield next_date
