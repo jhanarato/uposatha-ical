8584a9bdb5d1861138d092c1a002e676a2b7d593
Ajahn Jhanarato <jhanarato@gmail.com>
2022-12-29 09:28:21 +0800

    Created generate_uposatha_dates()

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,14 +1,25 @@
 from datetime import date
 
+import pytest
+
 from generate import uposatha_lengths, generate_event, generate_season_with_one_event
+from generate import generate_uposatha_dates
 
 def test_uposatha_lengths():
     assert uposatha_lengths() == [15, 15, 14, 15, 15, 15, 14, 15]
 
 def test_generate_event():
-    event = generate_event(date(2022, 12, 29))
+    event = generate_event(on_date=date(2022, 12, 29))
     assert event.date == date(2022, 12, 29)
 
 def test_generate_season_with_one_event():
     season = generate_season_with_one_event(event_date=date(2022, 12, 29))
     assert len(season.events) == 1
+
+def test_generate_first_uposatha_date():
+    dates = generate_uposatha_dates(day_before_season=date(2022, 12, 29))
+    assert dates[0] == date(2023, 1, 13)
+
+def test_generate_eight_uposathas():
+    dates = generate_uposatha_dates(day_before_season=date(2022, 12, 29))
+    assert len(dates) == 8
