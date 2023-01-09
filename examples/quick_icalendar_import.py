a1d87bf63f8615cf2912c0ce6ce9bf789586d7cf
Ajahn Jhanarato <jhanarato@gmail.com>
2023-01-09 10:44:00 +0800

    Quick import accepts an optional file name.

--- examples/quick_icalendar_import.py
+++ examples/quick_icalendar_import.py
@@ -1,9 +1,9 @@
 import icalendar
 from forest_sangha_moons.MahanikayaCalendar import MahanikayaCalendar
 
-def import_calendar():
+def import_calendar(ical_file="mahanikaya.ical"):
     """ For use in python console. Quickly import the icalendar file for exploration """
-    with open("mahanikaya.ical", "r") as f:
+    with open(ical_file, "r") as f:
         content = f.read()
 
     ical = icalendar.Calendar.from_ical(content)
