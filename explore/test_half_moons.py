473577bd29fe439a3d9a12738bfaaed12f50e6aa
Ajahn Jhanarato <jhanarato@gmail.com>
2023-01-17 10:05:33 +0800

    Removed 2 redundant tests.

--- explore/test_half_moons.py
+++ explore/test_half_moons.py
@@ -1,18 +1,4 @@
-import pytest
-from calendar_extensions import add_date_before
 from half_moons import *
-
-def test_all_seasons_in_normal_year(seasons_including_date_before, normal_years):
-    for season in seasons_including_date_before:
-        if season.end_year in normal_years:
-            assert days_between_half_moons(season) == [8, 15, 15, 14, 15, 15, 15, 14]
-
-def test_extra_month_in_hot_season(seasons_including_date_before, extra_month_years):
-    seasons = [ season for season in seasons_including_date_before if season.season_name == "Gimha"]
-    seasons = [season for season in seasons if season.end_year in extra_month_years]
-
-    for season in seasons:
-        assert days_between_half_moons(season) == [8, 15, 15, 14, 15, 15, 15, 14, 15, 15]
 
 def test_cold_seasons(cold_seasons):
     for season in cold_seasons:
